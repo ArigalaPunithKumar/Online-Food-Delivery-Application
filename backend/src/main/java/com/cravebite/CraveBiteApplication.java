723f2f4480181314490f4854a62324a6c7d87317
A Punith Kumar <arigalapunith0081@gmail.com>
2026-09-26 16:50:19 +0530

    Replace Node backend with Spring Boot backend

---
+++ backend/src/main/java/com/cravebite/CraveBiteApplication.java
@@ -0,0 +1,7 @@
+package com.cravebite;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class CraveBiteApplication {
+ public static void main(String[] args){SpringApplication.run(CraveBiteApplication.class,args);}
+}
